bdc835213d66e67df84411c8432d1d9fd6551855
Kako Ferrare <kakoferrare87@gmail.com>
2023-02-22 23:53:22 -0300

    chore: remove unused dependencies

--- Application/SimianApplication/Service/Services/SimianDnaVerifyService.cs
+++ Application/SimianApplication/Service/Services/SimianDnaVerifyService.cs
@@ -1,11 +1,6 @@
 ﻿using Domain.DTO;
 using Domain.Interfaces.Services;
 using Service.Abstractions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Service.Services
 {
