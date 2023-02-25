064010400447ab58a2d2f5f4e240523af285b79c
Kako Ferrare <kakoferrare87@gmail.com>
2023-02-25 01:23:46 -0300

    chore: remove coment

--- Application/SimianApplication/Domain/DTO/IsSimianDTO/IsSimianRequestDTO.cs
+++ Application/SimianApplication/Domain/DTO/IsSimianDTO/IsSimianRequestDTO.cs
@@ -5,11 +5,5 @@ namespace Domain.DTO.IsSimianDTO
     public class IsSimianRequestDTO
     {
         public string[] Dna { get; set; }
-
-        /*public IsSimianRequestDTO(string[] dna)
-        {
-            Dna = dna;
-            _ = ValidateAsync(this, new IsSimianValidator());
-        }*/
     }
 }
